f1c4869aa84b9f9b0d1995b8532f291517ca18a1
Anita <24868560+g-anita@users.noreply.github.com>
2019-01-26 23:46:21 -0500

    Updates

---
+++ precache-manifest.35ef15341cad523b5af49197f0489b14.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "288fddf3cc3c2a29e3cb",
+    "url": "/react-image-search-app/static/css/main.009aaf6c.chunk.css"
+  },
+  {
+    "revision": "288fddf3cc3c2a29e3cb",
+    "url": "/react-image-search-app/static/js/main.288fddf3.chunk.js"
+  },
+  {
+    "revision": "4c1a7783079f3cde3eed",
+    "url": "/react-image-search-app/static/js/1.4c1a7783.chunk.js"
+  },
+  {
+    "revision": "33cf5033356c60f67af1",
+    "url": "/react-image-search-app/static/js/runtime~main.33cf5033.js"
+  },
+  {
+    "revision": "0fa33abaf5ac4649ab259785e5fddb3d",
+    "url": "/react-image-search-app/index.html"
+  }
+];
